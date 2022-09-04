8fa759dc8f50d59d4e7ddebac1f6f5df8dce51cc
joyuna <joayounga@gmail.com>
2022-09-04 15:28:41 +0900

    22.09.03

---
+++ yuna-app/app/src/main/java/yuna/app/App1.java
@@ -0,0 +1,11 @@
+package yuna.app;
+
+public class App1 {
+
+  public static void main(String[] args) {
+    System.out.println("[게시판 애플리케이션]");
+
+
+  }
+
+}
---
+++ yuna-app/yuna-app/src/app004.java
@@ -0,0 +1,34 @@
+public class app004 {
+    public static void main(String[] args) {
+        System.out.println("[비행기타고슝 컴퍼니]");
+        System.out.println();
+        System.out.println("환영합니다!");
+        System.out.println();
+
+        int no = 0;
+        String title = "";
+        String content = "";
+        String writer = "";
+        String password = "";
+        int viewCount = 0;
+        long createdDate = 0;
+
+
+            System.out.println("메뉴");
+            System.out.println("1. 예약 목록");
+            System.out.println("2. 예약 상세보기");
+            System.out.println("3. 예약 등록");
+            System.out.println();
+            System.out.println("메뉴를 선택하세요[1..3])(0: 종료)");
+
+            int menuNo = keyboardput.nextInt();
+            keyboardInput.nextLine();
+
+            if(memuNo == 1) {
+                System.out.println("[예약 목록]");
+                ㅣ
+            } else;
+
+
+    }
+}
