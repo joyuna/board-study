100fdaf6fd1dbe166e8b9f8ea56b63bc025ea037
joyuna <joayounga@gmail.com>
2022-09-18 18:11:12 +0900

    board-app ver3. 클래스 정정, 삭제 및 while,if문 추가

--- bitcamp/board-app/app-001/src/main/java/com/bitcamp/board/BoardListApp.java
+++ bitcamp/board-app/app-001/src/main/java/com/bitcamp/board/BoardListApp.java
@@ -10,22 +10,8 @@ public class BoardListApp {
     System.out.println("[게시판 목록]");
     System.out.println("번호" + "\t" + "제목" + "\t" + "조회수" + "\t" + "작성자" + '\t' + "등록");
 
-    System.out.print(1);
-    System.out.print("\t");
-    System.out.print("제목입니다1");
-    System.out.print('\t');
-    System.out.print(20 + "\t");
-    System.out.print("홍길동\t");
-    System.out.print("2022-09-18\r\n");
 
 
-    System.out.print(2 + "\t" + "제목입니다2\t" +
-        11 + "\t" + "홍길동\t" + "2022-09-18\n");
 
-    System.out.println(3 + "\t제목입니다3\t" 
-        + 31 + "\t" + "임꺽정\t2022-09-18");
-
-    System.out.printf("%d\t%s\t%d\t%s\t%s\n",
-        4, "제목입니다4", 45 , "유관순", "2022-09-18");
   }
 }
