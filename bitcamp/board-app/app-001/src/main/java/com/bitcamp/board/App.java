21024666f6ebd76f086c698b56dc634920399943
joyuna <joayounga@gmail.com>
2022-09-18 16:27:01 +0900

    board-app ver2

--- bitcamp/board-app/app-001/src/main/java/com/bitcamp/board/App.java
+++ bitcamp/board-app/app-001/src/main/java/com/bitcamp/board/App.java
@@ -1,6 +1,6 @@
 /*
  * 게시판 관리 애플리케이션
- * 비트캠프-20220918-ver1
+ * 비트캠프-20220918-ver2
  */
 package com.bitcamp.board;
 
@@ -8,5 +8,6 @@ public class App {
 
   public static void main(String[] args) {
     System.out.println("게시판 애플리케이션!");
+    System.out.println("환영합니다!");
   }
 }
---
+++ bitcamp/board-app/app-001/src/main/java/com/bitcamp/board/BoardDetailApp.java
@@ -0,0 +1,19 @@
+/*
+ * 게시판 관리 애플리케이션
+ * 비트캠프-20220918-ver2
+ */
+package com.bitcamp.board;
+
+public class BoardDetailApp {
+
+  public static void main(String[] args) {
+    System.out.println("[게시판 상세보기]");
+
+    System.out.printf("번호: %d\n", 1);
+    System.out.printf("제목: %s\n", "제목입니다1.");
+    System.out.printf("내용: %s\n", "내용입니다.");
+    System.out.printf("조회수: %d\n", "100");
+    System.out.printf("작성자: %s\n", "홍길동");
+    System.out.printf("등록일: %s\n", "2022-09-18");
+  }
+}
