7e5756fe9d6c382e762c8e9a48ae8134b9347ff5
joyuna <joayounga@gmail.com>
2022-09-18 20:59:48 +0900

    board-app ver4. 키보드 입력값으로 게시글 등록 및 게시글 상세보기

--- bitcamp/board-app/app-001/src/main/java/com/bitcamp/board/App.java
+++ bitcamp/board-app/app-001/src/main/java/com/bitcamp/board/App.java
@@ -1,6 +1,6 @@
 /*
  * 게시판 관리 애플리케이션
- * 비트캠프-20220918-ver3
+ * 비트캠프-20220918-ver4
  */
 package com.bitcamp.board;
 
@@ -14,12 +14,22 @@ public class App {
 
     java.util.Scanner keyboardInput = new java.util.Scanner(System.in);
 
+    int no = 0;
+    String title = "";
+    String content = "";
+    String writer = "";
+    String password = "";
+    int viewCount = 0;
+    long createdDate = 0;
+
+
     while (true) {
       System.out.println("메뉴");
       System.out.println(" 1: 게시글 목록");
       System.out.println(" 2: 게시글 상세보기");
+      System.out.println(" 3: 게시글 등록");
       System.out.println();
-      System.out.println("메뉴를 선택하세요[1..2](0 :종료)");
+      System.out.println("메뉴를 선택하세요[1..3](0 :종료)");
 
       int menuNo = keyboardInput.nextInt();
       keyboardInput.nextLine();
@@ -49,12 +59,35 @@ public class App {
       } else if (menuNo == 2) {
         System.out.println("[게시판 상세보기]");
 
-        System.out.printf("번호: %d\n", 1);
-        System.out.printf("제목: %s\n", "제목입니다1.");
-        System.out.printf("내용: %s\n", "내용입니다.");
-        System.out.printf("조회수: %d\n", 100);
-        System.out.printf("작성자: %s\n", "홍길동");
-        System.out.printf("등록일: %s\n", "2022-09-18");
+        System.out.printf("번호: %d\n", no);
+        System.out.printf("제목: %s\n", title);
+        System.out.printf("내용: %s\n", content);
+        System.out.printf("조회수: %d\n", viewCount);
+        System.out.printf("작성자: %s\n", writer);
+
+        java.util.Date date = new java.util.Date(createdDate);
+
+        System.out.printf("등록일: %tY-%1$tm-%1$td %1$tH:%1$tM\n", date);
+
+      } else if (menuNo == 3) {
+        System.out.println("[게시글 등록]");
+
+        System.out.println("제목?");
+        title = keyboardInput.nextLine();
+
+        System.out.println("내용?");
+        content = keyboardInput.nextLine();
+
+        System.out.println("작성자?");
+        writer = keyboardInput.nextLine();
+
+        System.out.println("암호?");
+        password = keyboardInput.nextLine();
+
+        no = 1;
+        viewCount = 0;
+        createdDate = System.currentTimeMillis(); // 이 친구를 넣으니까 현재 시간이 출력 됨.
+
       } else {
         System.out.println("메뉴 번호가 옳지 않습니다!");
       }
