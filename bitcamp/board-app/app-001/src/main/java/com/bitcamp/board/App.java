083e10ceb7e492a3c1e91b3791295d413f109d16
joyuna <joayounga@gmail.com>
2022-09-18 15:44:39 +0900

    board-app ver1 날짜정정

--- bitcamp/board-app/app-001/src/main/java/com/bitcamp/board/App.java
+++ bitcamp/board-app/app-001/src/main/java/com/bitcamp/board/App.java
@@ -1,12 +1,12 @@
 /*
  * 게시판 관리 애플리케이션
- * 비트캠프-20220704
+ * 비트캠프-20220918-ver1
  */
 package com.bitcamp.board;
 
 public class App {
 
-    public static void main(String[] args) {
-        System.out.println("게시판 애플리케이션!");
-    }
+  public static void main(String[] args) {
+    System.out.println("게시판 애플리케이션!");
+  }
 }
